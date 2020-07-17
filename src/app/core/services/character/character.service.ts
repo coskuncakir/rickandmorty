8f2a69033a8dd49d2cc48190c55dfa4e49abb5ed
coskun cakir <csknckr@gmail.com>
2020-07-18 00:35:11 +0100

    env fix

--- src/app/core/services/character/character.service.ts
+++ src/app/core/services/character/character.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,6 @@ export class CharacterService {
   constructor(protected http: HttpClient) {}
 
   characters(): Observable<any> {
-    return this.http.get<any>('api/character');
+    return this.http.get(environment.api + '/character');
   }
 }
